c6de2ab13cbf725718808c847a3341c461e3d670
idstack <idstack@outlook.co.id>
2020-03-26 09:57:48 +0000

    create chat component

--- public/js/app.js
+++ public/js/app.js
@@ -1932,6 +1932,66 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  mounted: function mounted() {
+    console.log('Component mounted.');
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/bootstrap/dist/js/bootstrap.js":
 /*!*****************************************************!*\
   !*** ./node_modules/bootstrap/dist/js/bootstrap.js ***!
@@ -37334,6 +37394,97 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054&":
+/*!*********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054& ***!
+  \*********************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _vm._m(0)
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "container" }, [
+      _c("div", { staticClass: "row justify-content-center" }, [
+        _c("div", { staticClass: "col-md-8" }, [
+          _c("div", { staticClass: "card" }, [
+            _c("div", { staticClass: "card-header" }, [_vm._v("Chat Room")]),
+            _vm._v(" "),
+            _c(
+              "div",
+              {
+                staticClass: "card-body bg-secondary",
+                attrs: { id: "chat-box" }
+              },
+              [
+                _c("div", { staticClass: "media bg-light rounded" }, [
+                  _c("div", { staticClass: "media-body m-2" }, [
+                    _c("h5", { staticClass: "mt-0" }, [_vm._v("Username")]),
+                    _vm._v(
+                      "\n                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi nisi, nemo cupiditate tenetur laborum amet incidunt quasi voluptates earum, expedita officiis, adipisci reiciendis eaque harum sunt aspernatur vel vitae sit.\n                        "
+                    )
+                  ])
+                ])
+              ]
+            ),
+            _vm._v(" "),
+            _c("div", { staticClass: "card-footer" }, [
+              _c("div", { staticClass: "form-group" }, [
+                _c("input", {
+                  staticClass: "form-control",
+                  attrs: { type: "text" }
+                })
+              ]),
+              _vm._v(" "),
+              _c("p", { staticClass: "text-muted" }, [
+                _vm._v("Username typing...")
+              ])
+            ])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-4" }, [
+          _c("div", { staticClass: "card" }, [
+            _c("div", { staticClass: "card-header" }, [_vm._v("Users Online")]),
+            _vm._v(" "),
+            _c(
+              "div",
+              {
+                staticClass: "card-body bg-secondary",
+                attrs: { id: "user-online" }
+              },
+              [
+                _c("ul", { staticClass: "list-group" }, [
+                  _c("li", { staticClass: "list-group-item" }, [
+                    _vm._v("Username")
+                  ])
+                ])
+              ]
+            )
+          ])
+        ])
+      ])
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
 /*!********************************************************************!*\
   !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
@@ -49517,6 +49668,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+Vue.component('chatroom-component', __webpack_require__(/*! ./components/chat/RoomComponent.vue */ "./resources/js/components/chat/RoomComponent.vue")["default"]);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -49643,6 +49795,75 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/components/chat/RoomComponent.vue":
+/*!********************************************************!*\
+  !*** ./resources/js/components/chat/RoomComponent.vue ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./RoomComponent.vue?vue&type=template&id=28e62054& */ "./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054&");
+/* harmony import */ var _RoomComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./RoomComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _RoomComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/chat/RoomComponent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js&":
+/*!*********************************************************************************!*\
+  !*** ./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_RoomComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./RoomComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/chat/RoomComponent.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_RoomComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054&":
+/*!***************************************************************************************!*\
+  !*** ./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054& ***!
+  \***************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./RoomComponent.vue?vue&type=template&id=28e62054& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/chat/RoomComponent.vue?vue&type=template&id=28e62054&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_RoomComponent_vue_vue_type_template_id_28e62054___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -49650,7 +49871,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n8 │ @import '~bootstrap/scss/bootstrap';\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  /Users/idstack/Code/laravel/chat/resources/sass/app.scss 8:9  root stylesheet\n    at /Users/idstack/Code/laravel/chat/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/idstack/Code/laravel/chat/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/idstack/Code/laravel/chat/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/idstack/Code/laravel/chat/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Users/idstack/Code/laravel/chat/node_modules/sass-loader/dist/index.js:73:7\n    at Function.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:87203:16)\n    at _render_closure1.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:76994:12)\n    at _RootZone.runBinary$3$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25521:18)\n    at _RootZone.runBinary$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25525:19)\n    at _FutureListener.handleError$1 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23975:19)\n    at _Future__propagateToListeners_handleError.call$0 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:24271:40)\n    at Object._Future__propagateToListeners (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3500:88)\n    at _Future._completeError$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:24099:9)\n    at _AsyncAwaitCompleter.completeError$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23491:12)\n    at Object._asyncRethrow (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3256:17)\n    at /Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:13326:20\n    at _wrapJsFunctionForAsync_closure.$protected (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3279:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23512:12)\n    at _awaitOnObject_closure0.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23504:25)\n    at _RootZone.runBinary$3$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25521:18)\n    at _RootZone.runBinary$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25525:19)\n    at _FutureListener.handleError$1 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23975:19)\n    at _Future__propagateToListeners_handleError.call$0 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:24271:40)\n    at Object._Future__propagateToListeners (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3500:88)\n    at _Future._completeError$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:24099:9)\n    at _AsyncAwaitCompleter.completeError$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23491:12)\n    at Object._asyncRethrow (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3256:17)\n    at /Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:15981:20\n    at _wrapJsFunctionForAsync_closure.$protected (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3279:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23512:12)\n    at _awaitOnObject_closure0.call$2 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23504:25)\n    at _RootZone.runBinary$3$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25521:18)\n    at _RootZone.runBinary$3 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:25525:19)\n    at _FutureListener.handleError$1 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:23975:19)\n    at _Future__propagateToListeners_handleError.call$0 (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:24271:40)\n    at Object._Future__propagateToListeners (/Users/idstack/Code/laravel/chat/node_modules/sass/sass.dart.js:3500:88)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
---
+++ resources/js/components/chat/RoomComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Chat Room</div>
+
+                    <div id="chat-box" class="card-body bg-secondary">
+                        <div
+                            class="media bg-light rounded"
+                        >
+                            <div class="media-body m-2">
+                                <h5 class="mt-0">Username</h5>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi nisi, nemo cupiditate tenetur laborum amet incidunt quasi voluptates earum, expedita officiis, adipisci reiciendis eaque harum sunt aspernatur vel vitae sit.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="form-group">
+                            <input type="text"
+                                class="form-control"
+                            >
+                        </div>
+                        <p class="text-muted">Username typing...</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Users Online</div>
+                    <div id="user-online" class="card-body bg-secondary">
+                        <ul class="list-group">
+                            <li class="list-group-item">Username</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
